36a51718a90fa9d9420651dff27ae8fddccfd7e7
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-01-26 02:16:08 -0600

    push

---
+++ db.js
@@ -0,0 +1,36 @@
+const mysql=require("mysql");
+
+
+//async database constructor
+class dbConstructor {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+//https://codeburst.io/node-js-mysql-and-promises-4c3be599909b
+//constructor creates a new mysql connection with this configuration
+    query(sql, args) {
+        return new Promise ((resolve, reject) => {
+            this.connection.query(sql, args, (error, rows) => {
+                if (error) {
+                    console.log(error.sql)
+                    reject(error)
+                } else {
+                    resolve(rows)
+                }
+            })
+        })
+    }
+    close () {
+        return new Promise((resolve, reject) => {
+            this.connection.end(error => {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve();
+                }
+            })
+        })
+    }
+};
+
+module.exports = dbConstructor;
